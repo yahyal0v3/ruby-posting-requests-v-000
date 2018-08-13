f1ad455adb268a507c6c05710fc6c7d0eda532ca
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-08-13 19:41:54 +0000

    automatically backed up by learn

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -11,10 +11,10 @@ class TipsController < ApplicationController
   end
 
   def index
-  resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
-    req.params['oauth_token'] = session[:token]
-    req.params['v'] = '20160201'
+    resp = Faraday.get("https://api.foursquare.com/v2/lists/self/tips") do |req|
+      req.params['oauth_token'] = session[:token]
+      req.params['v'] = '20160201'
+    end
+    @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
   end
-  @results = JSON.parse(resp.body)["response"]["list"]["listItems"]["items"]
-end
 end
